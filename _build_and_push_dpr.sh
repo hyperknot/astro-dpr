4674ed492b0017f887224b50a5fe42301d3d6f34
Zsolt Ero <zsolt.ero@gmail.com>
2025-07-25 01:38:17 +0200

    work

--- _build_and_push_dpr.sh
+++ _build_and_push_dpr.sh
@@ -3,17 +3,19 @@
 git clean -xdf
 
 (cd packages/astro || exit
-  npm pkg set name="astro"
-  npm version patch --no-git-tag-version
+  pnpm pkg set name="astro"
+  pnpm version patch --no-git-tag-version  --workspaces-update=false
 )
 
 
 pnpm install
 pnpm build
 
+
 (cd packages/astro || exit
-  npm pkg set name="@hyperknot/astro-dpr"
-  npm publish --access public --provenance false
-  npm pkg set name="astro"
+  pnpm pkg set name="@hyperknot/astro-dpr"
+  pnpm publish --access public --provenance false --no-git-checks
+  pnpm pkg set name="astro"
 )
 
+git restore --staged --worktree pnpm-lock.yaml
